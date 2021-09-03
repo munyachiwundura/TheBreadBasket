d19f8d638dbafd03c90b453f55e5c75b20039cc9
munyachiwundura <90007302+munyachiwundura@users.noreply.github.com>
2021-09-03 02:28:20 +0200

    Update settings.py

--- breadbasket/breadbasket/settings.py
+++ breadbasket/breadbasket/settings.py
@@ -89,7 +89,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WiteNoiseMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
