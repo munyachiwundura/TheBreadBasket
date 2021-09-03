58b4f81da0fc7e30fc3ae3f5157dba6cd75e24b2
munyachiwundura <90007302+munyachiwundura@users.noreply.github.com>
2021-09-03 02:30:47 +0200

    Update settings.py

--- breadbasket/breadbasket/settings.py
+++ breadbasket/breadbasket/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-gzj6fnaabve3li(q$9b^_pl-abj309qo6&4(p9mad7n2$_j_x5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://thebreadbasket.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['thebreadbasket.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
